83006357cbc393521117866c8f481ce9cca8cbcb
Tuan Anh Le <anhlt1983@gmail.com>
2018-05-29 21:09:12 +0700

    add all stupid

--- client/src/index.js
+++ client/src/index.js
@@ -1,8 +1,22 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+// Readers: To prepare this project to build along in `./App.js`, complete
+// steps 1 & 2 below
+import React from "react";
+import ReactDOM from "react-dom";
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+import { BrowserRouter as Router } from "react-router-dom";
+
+import App from './components/App';
+
+import "./styles/index.css";
+import "./semantic-dist/semantic.css";
+
+// [STEP 1] Comment out this line:
+import './index-complete';
+
+// [STEP 2] Un-comment these lines:
+// ReactDOM.render(
+//   <Router>
+//     <App />
+//   </Router>,
+//   document.getElementById("root")
+// );
